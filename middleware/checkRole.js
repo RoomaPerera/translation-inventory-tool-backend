c34b0e5075a7013dbe6734bfa593147cd63dd915
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2025-05-17 16:43:10 +0530

    create checkRole middleware

---
+++ middleware/checkRole.js
@@ -0,0 +1,17 @@
+// Checks if the user's role is allowed to access the route
+
+const checkRole = (allowedRoles) => {
+
+    return (req, res, next) => {
+
+        const role = req.query.userRole;
+
+        if (!allowedRoles.includes(role)) {
+
+            return res.status(403).json({ message: 'Forbidden: Role not allowed' });
+        }
+        next();
+    };
+};
+
+module.exports = checkRole;
