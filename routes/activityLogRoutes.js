bbfd7284ad8b955c25418edc7a42074f99a0ea75
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2025-05-19 07:57:13 +0530

    update some parts

--- routes/activityLogRoutes.js
+++ routes/activityLogRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { addActivityLog , getActivityLogs } = require('../controllers/activityLogController');
+const { addActivityLog, getActivityLogs } = require('../controllers/activityLogController');
 const checkRole = require('../middleware/checkRole'); 
 
 
-// POST: Add a new activity
 router.post('/', addActivityLog);
 
-// GET: Get activity logs
+
 router.get('/', checkRole(['admin', 'developer', 'translator']), getActivityLogs);
 
 module.exports = router;
